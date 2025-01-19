917ec0bc925b57068d8234d714fcb6c2870a5ba4
nbr <nikolaus.brunner@protonmail.ch>
2025-01-19 14:53:24 +0100

    feat: setup serverless env

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,15 +1,13 @@
 "use server";
 
-// Docs: https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations
-
-import { PrismaClient } from "@prisma/client";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "../../db/prisma";
+
+// Docs: https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
